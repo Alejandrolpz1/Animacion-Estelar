6c2236cbe7a1ece0831c993b135e28c8fc22a0e4
Alejandrolpz1 <145937809+Alejandrolpz1@users.noreply.github.com>
2024-06-08 21:11:18 -0600

    commit beta

---
+++ src/js/scripts.js
@@ -0,0 +1,54 @@
+import * as THREE from 'three';
+
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+import { RGBELoader, RGBLoader } from 'three/examples/jsm/loaders/RGBELoader.js';
+
+const renderer = new THREE.WebGLRenderer({antialias: true});
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(45,window.innerWidth / window.innerHeight, 0.1, 1000);
+
+renderer.setClearColor(0xA3A3A3);
+
+const orbit = new OrbitControls(camera,renderer.domElement);
+
+camera.position.set(6,6,6);
+orbit.update();
+
+const grid = new THREE.GridHelper(30,30);   
+scene.add(grid);
+
+const gltfLoader = new GLTFLoader();
+const rgbeloader = new RGBELoader();
+
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure= 4;
+
+rgbeloader.load('./assets')
+
+gltfLoader.load('./assets/scene.gltf', function(gltf){
+    const model = gltf.scene;
+    scene.add(model)
+})
+
+function animate(time){
+    renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
+window.addEventListener('resize', function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateMatrixWorld();
+    renderer.setSize(this.window.innerWidth, window.innerHeight);
+    
+})
